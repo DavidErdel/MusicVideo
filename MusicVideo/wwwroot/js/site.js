9c66d07b22806bbcce89f6447432ebb78402391f
DavidErdel <daviderdel@gmail.com>
2022-07-15 19:33:06 -0500

    Add project files.

---
+++ MusicVideo/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function newColor() {
+    document.getElementById("heaven_or_las_vegas").style.color = "blue";
+}
+
+function oldColor() {
+    document.getElementById("heaven_or_las_vegas").style.color = "red";
+}
