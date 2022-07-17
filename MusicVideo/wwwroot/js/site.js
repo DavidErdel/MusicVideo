c8a83b00ff4451a0e37dfbfa768e7ea5750593a9
DavidErdel <daviderdel@gmail.com>
2022-07-17 15:03:21 -0500

    Corrections

--- MusicVideo/wwwroot/js/site.js
+++ MusicVideo/wwwroot/js/site.js
@@ -1,12 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
-
-function newColor() {
+﻿function newColor() {
     document.getElementById("heaven_or_las_vegas").style.color = "blue";
 }
 
 function oldColor() {
     document.getElementById("heaven_or_las_vegas").style.color = "red";
+}
+
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
 }
